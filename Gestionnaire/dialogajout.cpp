a6c6c107b3a8225bc366947671af12c453c96ea7
Nicolas Coutenet <nicolas.coutenet@laposte.net>
2011-12-28 14:38:31 +0100

    début d'implémentation des fonctions de l'ajout d'un document

--- Gestionnaire/dialogajout.cpp
+++ Gestionnaire/dialogajout.cpp
@@ -19,6 +19,26 @@ DialogAjout::~DialogAjout()
     delete ui;
 }
 
+Manga DialogAjout::getManga()
+{
+    return _myManga;
+}
+
+Comic DialogAjout::getComic()
+{
+    return _myComic;
+}
+
+Roman DialogAjout::getRoman()
+{
+    return _myRoman;
+}
+
+Article DialogAjout::getArticle()
+{
+    return _myArticle;
+}
+
 void DialogAjout::on_pbCancel_clicked()
 {
     this->close();
@@ -77,4 +97,75 @@ void DialogAjout::on_comboBox_activated(QString name)
         ui->leEditor->hide();
         ui->leType->hide();
     }
+}
+
+void DialogAjout::on_leTitle_editingFinished()
+{
+    if (ui->comboBox->currentText() == "Manga")
+    {
+        _myManga.setTitle(ui->leTitle->text().toStdString());
+    }
+    else if (ui->comboBox->currentText() == "Comic")
+    {
+        _myComic.setTitle(ui->leTitle->text().toStdString());
+    }
+    else if (ui->comboBox->currentText() == "Roman")
+    {
+        _myRoman.setTitle(ui->leTitle->text().toStdString());
+    }
+    else if (ui->comboBox->currentText() == "Article")
+    {
+        _myArticle.setTitle(ui->leTitle->text().toStdString());
+    }
+}
+
+void DialogAjout::on_leAuthor_editingFinished()
+{
+    if (ui->comboBox->currentText() == "Manga")
+    {
+        _myManga.setAuthor(ui->leAuthor->text().toStdString());
+    }
+    else if (ui->comboBox->currentText() == "Comic")
+    {
+        _myComic.setAuthor(ui->leAuthor->text().toStdString());
+    }
+    else if (ui->comboBox->currentText() == "Roman")
+    {
+        _myRoman.setAuthor(ui->leAuthor->text().toStdString());
+    }
+    else if (ui->comboBox->currentText() == "Article")
+    {
+        _myArticle.setAuthor(ui->leAuthor->text().toStdString());
+    }
+}
+
+void DialogAjout::on_leTheme_editingFinished()
+{
+    _myArticle.setTheme(ui->leTheme->text().toStdString());
+}
+
+void DialogAjout::on_leDateParution_editingFinished()
+{
+    _myArticle.setDateParution(ui->leDateParution->text().toStdString());
+}
+
+void DialogAjout::on_leEditor_editingFinished()
+{
+    if (ui->comboBox->currentText() == "Manga")
+    {
+        _myManga.setEditor(ui->leEditor->text().toStdString());
+    }
+    else if (ui->comboBox->currentText() == "Comic")
+    {
+        _myComic.setEditor(ui->leEditor->text().toStdString());
+    }
+    else if (ui->comboBox->currentText() == "Roman")
+    {
+        _myRoman.setEditor(ui->leEditor->text().toStdString());
+    }
+}
+
+void DialogAjout::on_leType_editingFinished()
+{
+    _myManga.setType(ui->leType->text().toStdString());
 }
--- Gestionnaire/dialogajout.h
+++ Gestionnaire/dialogajout.h
@@ -2,6 +2,10 @@
 #define DIALOGAJOUT_H
 
 #include <QDialog>
+#include "manga.hh"
+#include "roman.hh"
+#include "article.hh"
+#include "comic.hh"
 
 namespace Ui {
     class DialogAjout;
@@ -14,6 +18,10 @@ class DialogAjout : public QDialog
 public:
     explicit DialogAjout(QWidget *parent = 0);
     ~DialogAjout();
+    Manga getManga();
+    Comic getComic();
+    Roman getRoman();
+    Article getArticle();
 
 private slots:
     void on_pbCancel_clicked();
@@ -22,8 +30,24 @@ private slots:
 
     void on_comboBox_activated(QString );
 
+    void on_leTitle_editingFinished();
+
+    void on_leAuthor_editingFinished();
+
+    void on_leTheme_editingFinished();
+
+    void on_leDateParution_editingFinished();
+
+    void on_leEditor_editingFinished();
+
+    void on_leType_editingFinished();
+
 private:
     Ui::DialogAjout *ui;
+    Manga _myManga;
+    Comic _myComic;
+    Roman _myRoman;
+    Article _myArticle;
 };
 
 #endif // DIALOGAJOUT_H
